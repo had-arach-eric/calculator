774ee4a7de4ed62aefe0bc60346ff9547d48a815
Eric Had Arach <hadarachericj@gmail.com>
2023-07-26 14:27:40 -0300

    Do HTML elements and styles.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,24 @@
+/*
+Una operación de calculadora constará de un número, un operador y otro número. Por ejemplo, 3 + 5. Crea tres variables para cada una de las partes de una operación de calculadora. Crea una variable para el primer número, el operador y el segundo número. Usarás estas variables para actualizar tu pantalla más tarde.
+
+Crea una nueva función operar que tome un operador y 2 números y luego llame a una de las funciones anteriores sobre los números.
+
+Crea una calculadora HTML básica con botones para cada dígito, cada una de las funciones anteriores y una tecla "Equals".
+
+Añade un botón "clear".
+
+Debes redondear las respuestas con decimales largos para que no desborden la pantalla.
+
+Pulsar = antes de introducir todos los números o un operador puede causar problemas.
+
+Pulsar "clear" debería borrar cualquier dato existente... asegúrese de que el usuario realmente empieza de cero después de pulsar "clear".
+
+Muestra un mensaje de error si el usuario intenta dividir por 0... ¡y no dejes que se bloquee la calculadora!
+
+Mérito extra
+Los usuarios pueden obtener números de coma flotante si hacen los cálculos necesarios para obtenerlos, pero todavía no pueden escribirlos. Añade un botón . y deja que los usuarios introduzcan decimales. Asegúrate de que no pueden escribir más de uno: 12.3.56.5. Es difícil hacer cuentas con estos números. (desactiva el botón de decimales si ya hay uno en la pantalla)
+*/
+
 function add(num1, num2) {
 
 }
